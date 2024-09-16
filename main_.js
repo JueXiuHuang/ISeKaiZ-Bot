ce93eba873e8399eb6a8eb070089ccacb54b3524
aaron545 <43293060+aaron545@users.noreply.github.com>
2024-09-16 15:39:47 +0800

    🐛 Add professionVerify

--- main_.js
+++ main_.js
@@ -22,6 +22,11 @@ function testLogger(msg, eventType) {
   console.log('--------------------------')
 }
 
+
+// function selectMenu(msg){
+
+// }
+
 client.on('messageCreate', async (message) => {
   if (message.channelId != channelId) return;
   if (message.author.username != 'Isekaid') return;
@@ -30,22 +35,46 @@ client.on('messageCreate', async (message) => {
   if (message.components.length > 0) {
 
     // 印出 components 的內容
-    console.log(message.components[0]?.components[0]);
+    // console.log(message.components[0]);
+    if (message.embeds[0].title == "Fishing") {
+      try {
+        message.clickButton({ X: 0, Y: 0})
+      } catch (err) {
+        console.log(err);
+      }
+    } 
+
+    if (message.embeds[0].description == "Choose the correct option...") {
+      const X      = '1284730320133951592'
+      const Fish   = '1284729698701541458'
+      const Mine   = '1285099666912055359'
+      const Forage = '1285094197271199774'
+      // message.components[0].components.forEach(Button => console.log(Button.emoji.id))
+
+      message.components[0].components.forEach((Button, index) => {
+        if (Button.emoji.id != X) {
+          try {
+            message.clickButton({ X: index, Y: 0})
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      })
+    }
+
     console.log('-------------------------------------------------')
 
-    const customId = message.components[0].components[0].customId
-
-    try {
-      await message.selectMenu(0, ['Shadowcave Depths'])
-        .then((response) => {
-          console.log('選擇成功', response);
-        })
-        .catch((error) => {
-            console.error('選擇失敗', error);
-        });
-    } catch (error){
-      console.log('try_error: ' + error);
-    }
+    // try {
+    //   await message.selectMenu(0, ['Shadowcave Depths'])
+    //     .then((response) => {
+    //       console.log('選擇成功', response);
+    //     })
+    //     .catch((error) => {
+    //         console.error('選擇失敗', error);
+    //     });
+    // } catch (error){
+    //   console.log('try_error: ' + error);
+    // }
 
   } else {
       console.log('此訊息沒有 components');
